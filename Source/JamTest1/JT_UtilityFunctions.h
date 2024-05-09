c0606c4ed59514229a4639abfeb53ec025a62f29
Korsun Andrey <korsun.andrew@yandex.ru>
2024-05-09 11:03:07 +0300

    Add BPFunctionLibrary

---
+++ Source/JamTest1/JT_UtilityFunctions.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "JT_UtilityFunctions.generated.h"
+
+UCLASS()
+class JAMTEST1_API UJT_UtilityFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
