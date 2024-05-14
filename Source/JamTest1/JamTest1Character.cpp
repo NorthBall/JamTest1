a0fb5ee3c59b368cc80c63148aeb3135bedbad9c
Northball <korsun.andrew@yandex.ru>
2024-05-14 19:53:44 +0300

    Final build

--- Source/JamTest1/JamTest1Character.cpp
+++ Source/JamTest1/JamTest1Character.cpp
@@ -146,8 +146,8 @@ void AJamTest1Character::SetupPlayerInputComponent(class UInputComponent* Player
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		//Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		//Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AJamTest1Character::Move);
